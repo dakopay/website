1aebc5e228b6c8e8ea3d6fba31e95e0f5c8ffa20
Aman Singh <besoeasy@users.noreply.github.com>
2022-09-16 01:33:50 +0530

    👑

--- src/components/wrap_s.vue
+++ src/components/wrap_s.vue
@@ -2,17 +2,9 @@
 	<section class="w-full px-8 py-20 bg-white xl:px-0">
 		<div class="max-w-6xl mx-auto">
 			<p class="mb-5 font-medium text-black uppercase">What Is RePa ?</p>
-			<div class="flex flex-col md:flex-row">
-				<div class="w-full pr-5 md:w-4/12 xl:pr-12">
-					<p class="text-base font-normal leading-none text-gray-700 md:text-lg">
-						RePa is a decentralized protocol built on Ethereum EVM, that allows users and merchants to carry out trustless recurring cryptocurrency payments using smart contracts.
-					</p>
-				</div>
-
-				<div class="w-full mt-5 md:mt-0 md:w-8/12 md:pl-2">
-					<h3 class="mb-5 -mt-1 text-3xl leading-tight">The atomic nature of blockchain makes these subscriptions easy to manage without country-based restrictions.</h3>
-				</div>
-			</div>
+			<p class="text-base font-normal leading-none text-gray-700 md:text-lg">
+				RePa is a decentralized protocol built on Ethereum EVM, that allows users and merchants to carry out trustless recurring cryptocurrency payments using smart contracts.
+			</p>
 		</div>
 	</section>
 </template>
