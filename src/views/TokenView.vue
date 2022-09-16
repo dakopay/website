24cd4aa5a5a4ac14ebc6b1950208b2ca66002f9a
Aman Singh <besoeasy@users.noreply.github.com>
2022-09-16 13:47:02 +0530

    bg

--- src/views/TokenView.vue
+++ src/views/TokenView.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<section class="w-full flex bg-gray-50">
+		<section class="w-full flex">
 			<div class="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
 				<div class="text-center space-y-5">
 					<p class="mt-1 text-4xl font-extrabold text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
