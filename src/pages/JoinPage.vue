ac2ff2dd55659e49bb61316c52ecc453cd6c86ee
Aman Singh <besoeasy@users.noreply.github.com>
2022-10-16 22:38:29 +0530

    isMobile

--- src/pages/JoinPage.vue
+++ src/pages/JoinPage.vue
@@ -1,49 +1,58 @@
 <template>
-	<div>LOADING...................</div>
+	<div class="py-40">
+		<section class="py-16 bg-white md:py-24">
+			<div class="flex flex-col max-w-6xl px-10 mx-auto lg:flex-row">
+				<div class="flex flex-col items-start justify-center w-full pr-10 mb-12 lg:w-2/3 lg:mb-0">
+					<div class="relative">
+						<p class="mb-2 text-base font-bold text-black uppercase" v-if="mobiledevice">Mobile Device Detected</p>
+						<h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
+							<span class="block xl:inline">CHOOSE</span><br /><span class="block text-pink-500 xl:inline">WALLET</span>
+						</h1>
+						<div class="py-5"></div>
+					</div>
+					<div class="relative flex items-center pt-4 space-x-3">
+						<a :href="metamask_url" class="relative text-2xl group"
+							><span class="relative z-10 px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg group-hover:bg-pink-400 group-hover:text-white">Metamask</span
+							><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span></a
+						><a :href="trustwallet_url" class="relative text-2xl group"
+							><span class="relative z-10 px-5 py-2 font-bold leading-tight text-green-900 bg-green-200 border-4 border-black rounded-lg group-hover:bg-green-400 group-hover:text-white"
+								>Trust Wallet</span
+							><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span
+						></a>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
 </template>
 
 <script>
-	import { isAndroid, isIOS } from 'mobile-device-detect';
-	import { decodeSubscription } from '@moneymafia/repa-sdk';
+	import { isMobile } from 'mobile-device-detect';
 
 	export default {
 		props: ['hash'],
 
 		data() {
 			return {
-				urltogo: '',
+				mobiledevice: false,
+				desktop_url: '',
+				trustwallet_url: '',
+				metamask_url: '',
 			};
 		},
 
 		async mounted() {
-			const decodedsub = await decodeSubscription(this.hash);
+			this.desktop_url = `https://join.repa.gg?hash=` + this.hash;
 
-			let sep44x = 9006;
+			this.trustwallet_url = `https://link.trustwallet.com/open_url?url=https://join.repa.gg?hash=` + this.hash;
 
-			if (decodedsub.network == 56) {
-				sep44x = 9006;
+			this.metamask_url = `https://metamask.app.link/dapp/join.repa.gg/?hash=` + this.hash;
+
+			this.mobiledevice = isMobile;
+
+			if (window.ethereum) {
+				window.location.href = this.desktop_url;
 			}
-
-			if (decodedsub.network == 137) {
-				sep44x = 966;
-			}
-
-			const desktop_url = `https://join.repa.gg?hash=`;
-
-			const mobile_url = `https://link.trustwallet.com/open_url?coin_id=` + sep44x + `&url=` + desktop_url;
-
-			if (isAndroid) {
-				console.log(mobile_url + this.hash);
-				this.urltogo = mobile_url + this.hash;
-			} else if (isIOS) {
-				console.log(mobile_url + this.hash);
-				this.urltogo = mobile_url + this.hash;
-			} else {
-				console.log(desktop_url + this.hash);
-				this.urltogo = desktop_url + this.hash;
-			}
-
-			window.location.href = this.urltogo;
 		},
 	};
 </script>
