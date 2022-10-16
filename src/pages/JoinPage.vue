1b66307715bbdf98152efbfc6b4594687499dca0
Aman Singh <besoeasy@users.noreply.github.com>
2022-10-16 19:22:48 +0530

    --

--- src/pages/JoinPage.vue
+++ src/pages/JoinPage.vue
@@ -1,29 +1,51 @@
 <template>
-	<div>LOADING...................</div>
+	<div>LOADING...................<br />{{ urltogo }}</div>
 </template>
 
 <script>
 	import { isAndroid, isIOS } from 'mobile-device-detect';
+	import { decodeSubscription } from '@moneymafia/repa-sdk';
 
 	export default {
 		props: ['hash'],
 
+		data() {
+			return {
+				urltogo: '',
+			};
+		},
+
 		async mounted() {
-			console.log('hash', this.hash);
+			const decodedsub = await decodeSubscription(this.hash);
+
+			let sep44x = '9006';
+
+			if (decodedsub.network == 56) {
+				sep44x = '9006';
+			}
+
+			if (decodedsub.network == 137) {
+				sep44x = '966';
+			}
 
 			const desktop_url = `https://join.repa.gg?hash=`;
-			const mobile_url = `https://link.trustwallet.com/open_url?url=` + desktop_url;
+
+			const mobile_url = `https://link.trustwallet.com/open_url?coin_id=` + sep44x + `&url=` + desktop_url;
 
 			if (isAndroid) {
 				console.log(mobile_url + this.hash);
-				window.location.href = mobile_url + this.hash;
+				this.urltogo = mobile_url + this.hash;
 			} else if (isIOS) {
 				console.log(mobile_url + this.hash);
-				window.location.href = mobile_url + this.hash;
+				this.urltogo = mobile_url + this.hash;
 			} else {
 				console.log(desktop_url + this.hash);
-				window.location.href = desktop_url + this.hash;
+				this.urltogo = desktop_url + this.hash;
 			}
+
+			this.urltogo = mobile_url + this.hash;
+
+			window.location.href = this.urltogo;
 		},
 	};
 </script>
