92c54743627f9f61b29eb62db169b8e4fe70187a
Aman Singh <besoeasy@users.noreply.github.com>
2022-10-17 03:27:41 +0530

    okay

--- src/pages/JoinPage.vue
+++ src/pages/JoinPage.vue
@@ -2,9 +2,14 @@
 	<section class="bg-white md:py-24 mx-auto max-w-6xl px-10 py-16">
 		<p class="mb-2 text-base font-bold text-black uppercase" v-if="mobiledevice">Mobile Device Detected</p>
 		<h1 class="text-4xl font-extrabold tracking-tight text-gray-900"><span class="block xl:inline">CHOOSE</span> <span class="block text-pink-500 xl:inline">WALLET</span></h1>
+		<h1 v-if="!web3connected" class="text-4xl font-extrabold tracking-tight text-gray-900">INSTALL METAMASK / WEB3 WALLET</h1>
+
 		<div class="py-10"></div>
 
 		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-3xl">
+			<div class="mt-5">
+				<a :href="desktop_url" class="px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg hover:bg-blue-400 hover:text-white">WEB3 WALLET</a>
+			</div>
 			<div class="mt-5">
 				<a :href="metamask_url" class="px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg hover:bg-blue-400 hover:text-white">METAMASK</a>
 			</div>
@@ -23,6 +28,7 @@
 
 		data() {
 			return {
+				web3connected: true,
 				mobiledevice: false,
 				desktop_url: '',
 				trustwallet_url: '',
@@ -41,6 +47,10 @@
 
 			if (window.ethereum) {
 				window.location.href = this.desktop_url;
+			} else {
+				if (!isMobile) {
+					this.web3connected = false;
+				}
 			}
 		},
 	};
