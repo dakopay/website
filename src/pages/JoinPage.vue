94c5e6703312fcc22d779006780d7db6cf55e51c
Aman Singh <besoeasy@users.noreply.github.com>
2022-10-16 22:44:32 +0530

    --

--- src/pages/JoinPage.vue
+++ src/pages/JoinPage.vue
@@ -1,29 +1,27 @@
 <template>
-	<div class="py-40">
-		<section class="py-16 bg-white md:py-24">
-			<div class="flex flex-col max-w-6xl px-10 mx-auto lg:flex-row">
-				<div class="flex flex-col items-start justify-center w-full pr-10 mb-12 lg:w-2/3 lg:mb-0">
-					<div class="relative">
-						<p class="mb-2 text-base font-bold text-black uppercase" v-if="mobiledevice">Mobile Device Detected</p>
-						<h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
-							<span class="block xl:inline">CHOOSE</span><br /><span class="block text-pink-500 xl:inline">WALLET</span>
-						</h1>
-						<div class="py-5"></div>
-					</div>
-					<div class="relative flex items-center pt-4 space-x-3">
-						<a :href="metamask_url" class="relative text-2xl group"
-							><span class="relative z-10 px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg group-hover:bg-pink-400 group-hover:text-white">Meta Mask</span
-							><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span
-						></a>
-						<a :href="trustwallet_url" class="relative text-2xl group"
-							><span class="relative z-10 px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg group-hover:bg-pink-400 group-hover:text-white">Trust Wallet</span
-							><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span
-						></a>
-					</div>
+	<section class="py-16 bg-white md:py-24">
+		<div class="flex flex-col max-w-6xl px-10 mx-auto lg:flex-row">
+			<div class="flex flex-col items-start justify-center w-full pr-10 mb-12 lg:w-2/3 lg:mb-0">
+				<div class="relative">
+					<p class="mb-2 text-base font-bold text-black uppercase" v-if="mobiledevice">Mobile Device Detected</p>
+					<h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl xl:text-6xl">
+						<span class="block xl:inline">CHOOSE</span><br /><span class="block text-pink-500 xl:inline">WALLET</span>
+					</h1>
+					<div class="py-5"></div>
+				</div>
+				<div class="relative flex items-center pt-4 space-x-3">
+					<a :href="metamask_url" class="relative text-2xl group"
+						><span class="relative z-10 px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg group-hover:bg-pink-400 group-hover:text-white">Meta Mask</span
+						><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span
+					></a>
+					<a :href="trustwallet_url" class="relative text-2xl group"
+						><span class="relative z-10 px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg group-hover:bg-pink-400 group-hover:text-white">Trust Wallet</span
+						><span class="absolute top-0 right-0 w-full h-14 -mt-2.5 -mr-0.5 bg-black rounded-lg"></span
+					></a>
 				</div>
 			</div>
-		</section>
-	</div>
+		</div>
+	</section>
 </template>
 
 <script>
