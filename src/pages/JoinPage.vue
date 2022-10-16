2fac985a42e8b5e5515cca4587e6b308b28ac462
Aman Singh Katal <besoeasy@users.noreply.github.com>
2022-10-17 03:56:44 +0530

    Update JoinPage.vue

--- src/pages/JoinPage.vue
+++ src/pages/JoinPage.vue
@@ -5,7 +5,7 @@
 		<h1 v-if="!web3connected" class="text-4xl font-extrabold tracking-tight text-gray-900">INSTALL METAMASK / WEB3 WALLET</h1>
 		<div class="py-10"></div>
 
-		<div class="grid grid-cols-1 md:grid-cols-2 gap-4 text-3xl">
+		<div class="grid grid-cols-1 md:grid-cols-2 lg:md:grid-cols-3 gap-4 text-3xl">
 			<div class="mt-5" v-if="!mobiledevice">
 				<a :href="desktop_url" class="px-5 py-2 font-bold leading-tight text-black bg-white border-4 border-gray-900 rounded-lg hover:bg-blue-400 hover:text-white">WEB3 WALLET</a>
 			</div>
