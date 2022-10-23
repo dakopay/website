113968fca61cbc217df8a6cb0edfdab29efc0f7b
Aman Singh <besoeasy@users.noreply.github.com>
2022-10-23 19:05:46 +0530

    ..

--- src/components/feature_now.vue
+++ src/components/feature_now.vue
@@ -2,7 +2,7 @@
 	<section class="relative w-full py-12 overflow-hidden bg-white md:py-20 xl:pt-32 xl:pb-40">
 		<div class="container relative flex flex-col justify-between h-full px-10 mx-auto xl:px-0">
 			<div class="flex flex-col w-full mb-10 sm:flex-row">
-				<div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+				<div class="w-full md:w-1/4">
 					<div class="relative h-full ml-0 mr-0 sm:mr-10">
 						<span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-indigo-500 rounded-lg"></span>
 						<div class="relative h-full p-5 bg-white border-2 border-indigo-500 rounded-lg">
@@ -11,14 +11,14 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 								</svg>
 
-								<h3 class="my-5 ml-3 text-lg font-bold text-gray-800">Zero Database</h3>
+								<h3 class="my-5 ml-3 text-xl font-bold text-gray-800">Zero Database</h3>
 							</div>
 							<p class="mb-2 text-gray-600">All the subscriptions are stored and managed inside smart contract. No any off-chain storage is being used.</p>
 						</div>
 					</div>
 				</div>
 
-				<div class="w-full sm:w-1/2">
+				<div class="w-full md:w-1/4">
 					<div class="relative h-full ml-0 md:mr-10">
 						<span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-purple-500 rounded-lg"></span>
 						<div class="relative h-full p-5 bg-white border-2 border-purple-500 rounded-lg">
@@ -27,16 +27,14 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 								</svg>
 
-								<h3 class="my-5 ml-3 text-lg font-bold text-gray-800">Multichain</h3>
+								<h3 class="my-5 ml-3 text-xl font-bold text-gray-800">Multichain</h3>
 							</div>
 							<p class="mb-2 text-gray-600">We support multiple chains and you can use any of them. Currently we support BSC and MATIC chains.</p>
 						</div>
 					</div>
 				</div>
-			</div>
 
-			<div class="flex flex-col w-full mb-5 sm:flex-row">
-				<div class="w-full mb-10 sm:mb-0 sm:w-1/2">
+				<div class="w-full md:w-1/4">
 					<div class="relative h-full ml-0 mr-0 sm:mr-10">
 						<span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-blue-400 rounded-lg"></span>
 						<div class="relative h-full p-5 bg-white border-2 border-blue-400 rounded-lg">
@@ -45,14 +43,14 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 								</svg>
 
-								<h3 class="my-5 ml-3 text-lg font-bold text-gray-800">Easy integration</h3>
+								<h3 class="my-5 ml-3 text-xl font-bold text-gray-800">Easy integration</h3>
 							</div>
 							<p class="mb-2 text-gray-600">Create subscription and share link to the user. Once user start paying you, you have your plan enabled.</p>
 						</div>
 					</div>
 				</div>
 
-				<div class="w-full sm:w-1/2">
+				<div class="w-full md:w-1/4">
 					<div class="relative h-full ml-0 md:mr-10">
 						<span class="absolute top-0 left-0 w-full h-full mt-1 ml-1 bg-green-500 rounded-lg"></span>
 						<div class="relative h-full p-5 bg-white border-2 border-green-500 rounded-lg">
@@ -61,7 +59,7 @@
 									<path stroke-linecap="round" stroke-linejoin="round" d="M12.75 15l3-3m0 0l-3-3m3 3h-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 								</svg>
 
-								<h3 class="my-5 ml-3 text-lg font-bold text-gray-800">ERC20 Token</h3>
+								<h3 class="my-5 ml-3 text-xl font-bold text-gray-800">ERC20 Token</h3>
 							</div>
 							<p class="mb-2 text-gray-600">You can use any ERC-20 tokens on repa.gg, built open for all tokens.</p>
 						</div>
